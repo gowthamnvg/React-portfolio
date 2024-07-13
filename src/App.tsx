22ff00e88c4dfdae3f5b6ab59c8f1d6ab4133b1e
Gowtham <nvggowtham@gmail.com>
2024-07-13 17:16:54 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome by Gowtham
+          Welcome by Gowtham M
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
